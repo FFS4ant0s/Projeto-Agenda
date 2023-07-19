2b2175445324913c0fcd377c434499f434f1a000
Fernando Fernandes <121583831+FFS4ant0s@users.noreply.github.com>
2023-07-19 13:53:53 -0300

    Add files via upload

--- contact/admin.py
+++ contact/admin.py
@@ -4,12 +4,18 @@ from contact import models
 
 
 @admin.register(models.Contact)
-class ContractAdmin(admin.ModelAdmin):
+class ContactAdmin(admin.ModelAdmin):
     list_display = 'id', 'first_name', 'last_name', 'phone',
     ordering = '-id',
-    # list_filter = ('created_date',)
-    search_fields = 'id', 'first_name  ', 'last_name', #type:ignore
+    # list_filter = 'created_date',
+    search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = 'first_name', 'last_name'
-    list_display_links = 'id', 'phone'
+    list_editable = 'first_name', 'last_name',
+    list_display_links = 'id', 'phone',
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
